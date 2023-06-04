59f1ad3e0a2a3310a8266f3eaa615b686a07797c
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-06-04 10:38:37 +0500

    Making Intro Responsive

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,9 +5,9 @@ export default function Intro() {
     return (
         <div className='w-full h-full md:px-10 flex items-center text-white  flex-col justify-center relative'>
 
-            <h1 className='text-6xl my-2 font-semibold text-white/90 '>Clean home, happy life </h1>
-            <h1 className='text-6xl my-2  font-semibold text-white/70 '>Let LocalPro help</h1>
-            <button className='btn btn-lg btn-wide text-xl my-4  dark:text-white'>View Services </button>
+            <h1 className='md:text-6xl text-2xl my-2 font-semibold text-white/90 '>Clean home, happy life </h1>
+            <h1 className='md:text-6xl text-2xl my-2  font-semibold text-white/70 '>Let LocalPro help</h1>
+            <button className='btn md:btn-lg md:btn-wide text-xl my-4  dark:text-white'>View Services </button>
 
         </div>
     )
