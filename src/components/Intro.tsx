f2819cc727c7b98b100fe629376c12ed08e87317
Abdullah-moiz <mrmoiz.dev@gmail.com>
2023-06-04 10:30:54 +0500

    Intro Page and NavBar is ready

---
+++ src/components/Intro.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Intro() {
+    return (
+        <div className='w-full h-full md:px-10 flex items-center text-white  flex-col justify-center relative'>
+
+            <h1 className='text-6xl my-2 font-semibold text-white/90 '>Clean home, happy life </h1>
+            <h1 className='text-6xl my-2  font-semibold text-white/70 '>Let LocalPro help</h1>
+            <button className='btn btn-lg btn-wide text-xl my-4  dark:text-white'>View Services </button>
+
+        </div>
+    )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Navbar() {
+    return (
+        <div className="navbar fixed top-0 left-0 z-50 bg-slate-950 text-white ">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link href={"/"}>Item 1</Link></li>
+                        <li>
+                            <Link href={"/"}>Parent</Link>
+                            <ul className="p-2">
+                                <li><Link href={"/"}>Submenu 1</Link></li>
+                                <li><Link href={"/"}>Submenu 2</Link></li>
+                            </ul>
+                        </li>
+                        <li><Link href={"/"}>Item 3</Link></li>
+                    </ul>
+                </div>
+                <Link href={"/"} className="btn btn-ghost normal-case text-xl">LocalPro</Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><Link href={"/"}>Item 1</Link></li>
+                    <li tabIndex={0}>
+                        <details>
+                            <summary>Parent</summary>
+                            <ul className="p-2">
+                                <li><Link href={"/"}>Submenu 1</Link></li>
+                                <li><Link href={"/"}>Submenu 2</Link></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><Link href={"/"}>Item 3</Link></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <Link href={"/"} className="btn dark:text-white">Login</Link>
+            </div>
+        </div>
+    )
+}
